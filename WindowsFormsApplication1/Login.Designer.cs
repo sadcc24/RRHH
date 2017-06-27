1c7cd6a5c9a9e266068d9262e2fc7853b0db6348
mdouglas0905 <mdouglas090591@gmail.com>
2017-06-26 20:34:24 -0600

    se modifico campo password

--- WindowsFormsApplication1/Login.Designer.cs
+++ WindowsFormsApplication1/Login.Designer.cs
@@ -52,6 +52,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 1;
+            this.textBox2.UseSystemPasswordChar = true;
             // 
             // button1
             // 
